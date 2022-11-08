313100d06e83729406097f69117d648ec63b60de
Robin Maasjosthusmann <robin.maasjosthusmann@taod.de>
2022-11-08 11:18:35 +0100

    fix: Fix deprecated warnings after plugin updates

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -1,45 +1,5 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
--- There is some kind of refactoring/migration going on in the upstream
- -- project, so these Vim/ global settings are likely temporary and may return to
- -- the `setup(...)` configuration at some point in the future. Maybe?
- --
- -- https://github.com/kyazdani42/nvim-tree.lua/issues/674
- -- https://github.com/LunarVim/Neovim-from-scratch/issues/124
-
- -- Enables file highlight for Git attributes
- vim.g["nvim_tree_git_hl"] = 1
- -- In what format to show root folder
- vim.g["nvim_tree_root_folder_modifier"] = ":t"
- -- Icons to render
- vim.g["nvim_tree_show_icons"] = {
-   git = 1,
-   folders = 1,
-   files = 1,
-   folder_arrows = 1,
-   tree_width = 30,
- }
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
@@ -51,9 +11,58 @@ if not config_status_ok then
   return
 end
 
+-- Replaces auto_close
 local tree_cb = nvim_tree_config.nvim_tree_callback
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
 
 nvim_tree.setup {
+  renderer = {
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      },
+    },
+    highlight_git = true,
+    root_folder_modifier = ":t",
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
@@ -73,7 +82,7 @@ nvim_tree.setup {
       warning = "",
       error = "",
     },
- },
+  },
   update_focused_file = {
     enable = true,
     update_cwd = true,
@@ -114,8 +123,11 @@ nvim_tree.setup {
   },
   actions = {
     open_file = {
-      -- Cloeses tree when you open a file
-      quit_on_open = true
-    }
-  }
+      resize_window = true,
+      quit_on_open = true,
+      window_picker = {
+            enable = false,
+      },
+    },
+  },
 }
