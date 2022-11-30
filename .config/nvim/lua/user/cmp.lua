34fa9da5e4b15183d787d39359251bce0f5641f1
Robin Maasjosthusmann <robin.maasjosthusmann@taod.de>
2022-11-30 08:39:35 +0100

    Change order for tab completion

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -67,10 +67,10 @@ cmp.setup({
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			local copilot_keys = vim.fn["copilot#Accept"]()
-			if copilot_keys ~= "" and type(copilot_keys) == "string" then
-				vim.api.nvim_feedkeys(copilot_keys, "i", true)
-			elseif cmp.visible() then
+			if cmp.visible() then
 				cmp.select_next_item()
+			elseif copilot_keys ~= "" and type(copilot_keys) == "string" then
+					vim.api.nvim_feedkeys(copilot_keys, "i", true)
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
 			else
